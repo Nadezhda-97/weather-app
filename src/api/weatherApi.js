47b9ca3b06c4fb50e171841a26da280489ff6e2a
nnadya <n.d.niki2012@gmail.com>
2025-12-26 23:24:38 +0300

    add fetchWeather

--- src/api/weatherApi.js
+++ src/api/weatherApi.js
@@ -1,1 +1,16 @@
-//запросы к апи
+const apiKey = 'db1d0fea490bdb2fd095dcd7e3098257';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&lang=en';
+
+const fetchWeather = async (location) => {
+    const response = await fetch(`${apiUrl}&q=${location}&appid=${apiKey}`);
+
+    if (!response.ok) {
+        const error = new Error('Network response was not ok');
+        error.name = 'ResponseError';
+        throw error;
+    }
+
+    return response.json();
+};
+
+export default fetchWeather;
