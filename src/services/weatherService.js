df067095ac2f2073feca43074af1f79441d56da4
nnadya <n.d.niki2012@gmail.com>
2025-12-26 23:25:56 +0300

    add mapWeatherData

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,1 +1,20 @@
-//обработка данных с апи
+const mapWeatherData = (data) => {
+    const { humidity, pressure, temp, feels_like: feelsLike } = data.main;
+    const { description, icon } = data.weather[0];
+    const { speed: wind } = data.wind;
+
+    return {
+        location: data.name,
+        iconUrl: `http://openweathermap.org/img/wn/${icon}@2x.png`,
+        info: {
+            temperature: temp,
+            feelsLike,
+            humidity,
+            pressure,
+            description,
+            wind,
+        },
+    };
+};
+
+export default mapWeatherData;
